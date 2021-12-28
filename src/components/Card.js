23396d87c2bfa8472df5fcdc4156b9f4ba60debb
jaresj <justice.jares@gmail.com>
2021-12-28 11:46:21 -0800

    Added styling to cards component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
-export default function Card({Aname, Bname, date, Ascore, Bscore}) {
-    return (
-        <div>
-            <p>{Aname}</p>
-            <p>{Ascore}</p>
-            <p>{Bname}</p>
-            <p>{Bscore}</p>
-            <p>{date}</p>
-        </div>
-    )
+export default function Card({ Aname, Bname, date, Ascore, Bscore }) {
+  return (
+    <div className="gameCard">
+      <div className="gameTeams">
+        <p>{Aname}</p>
+        <p>{Ascore}</p>
+        <p>{Bname}</p>
+        <p>{Bscore}</p>
+      </div>
+      <p>{date}</p>
+    </div>
+  );
 }
