b6cc7d96a4a69f79697001b3f23304d74ab2c78a
Luke G <lukegronert@gmail.com>
2021-12-31 12:59:28 +0800

    Installed justice's dependencies. Created images folder with current team logos from the 4 major regions. Create data folder to hold team-info.js json object with team name (as written by sportsdata.io) and team logo. Create findLogo function in Card.js to place the logo for each team in Card. If no logo is found, places the League of Legends logo instead.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,37 @@
 import React from "react";
 import { format } from "date-fns";
+import {teams} from '../data/team-data';
+import leagueLogo from '../images/league-logo.png';
 
 export default function Card({ Aname, Bname, date, Ascore, Bscore }) {
+  // Function that finds team logo, returns nothing if no logo found
+  const findLogo = (teamLetter) => {
+    const logo = teams.find(team => team.name === teamLetter);
+    if(logo !== undefined) {
+      return logo.logo;
+    } else {
+      return leagueLogo;
+    }
+  }
+
+  const teamALogo = findLogo(Aname);
+  const teamBLogo = findLogo(Bname);
+
   return (
-    <div className="gameCard">
-      <div className="gameTeams">
-        <p>{Aname}</p>
-        <p>{Ascore}</p>
-        <p>{Bname}</p>
-        <p>{Bscore}</p>
+    <div className="game-card">
+      <div className="teams">
+        <div className='team-info'>
+          <p className='team-name'>{Aname}</p>
+          <p className='team-score'>{Ascore}</p>
+          <img alt='pic' src={teamALogo} />
+        </div>
+        <div className='team-info'>
+          <p className='team-name'>{Bname}</p>
+          <p className='team-score'>{Bscore}</p>
+          <img alt='pic' src={teamBLogo} />
+        </div>
       </div>
-      <div className="gameDate">
+      <div className="game-date">
         <p>{format(new Date(date), "MMMM do, Y")}</p>
       </div>
     </div>
---
+++ src/data/team-data.js
@@ -0,0 +1,249 @@
+// LCS Logos
+import dig from '../images/logos/LCS/dig.png';
+import tl from '../images/logos/LCS/tl.png';
+import hundredThieves from '../images/logos/LCS/hundred-thieves.png';
+import c9 from '../images/logos/LCS/c9.png';
+import clg from '../images/logos/LCS/clg.png';
+import eg from '../images/logos/LCS/eg.png';
+import fly from '../images/logos/LCS/fly.png';
+import gg from '../images/logos/LCS/gg.png';
+import imt from '../images/logos/LCS/imt.png';
+import tsm from '../images/logos/LCS/tsm.png';
+
+// LEC logos
+import mad from '../images/logos/LEC/mad.png'
+import g2 from '../images/logos/LEC/g2.png'
+import sk from '../images/logos/LEC/sk.png'
+import ast from '../images/logos/LEC/ast.png'
+import rge from '../images/logos/LEC/rge.png'
+import xl from '../images/logos/LEC/xl.png'
+import vit from '../images/logos/LEC/vit.png'
+import fnc from '../images/logos/LEC/fnc.png'
+import msf from '../images/logos/LEC/msf.png'
+import bds from '../images/logos/LEC/bds.png'
+
+// LCK logos
+import t1 from '../images/logos/LCK/t1.png';
+import af from '../images/logos/LCK/af.png';
+import bro from '../images/logos/LCK/bro.png';
+import drx from '../images/logos/LCK/drx.png';
+import dwg from '../images/logos/LCK/dwg.png';
+import geng from '../images/logos/LCK/geng.png';
+import hle from '../images/logos/LCK/hle.png';
+import kt from '../images/logos/LCK/kt.png';
+import lsb from '../images/logos/LCK/lsb.png';
+import ns from '../images/logos/LCK/ns.png';
+
+// LPL logos
+import fpx from '../images/logos/LPL/fpx.png'
+import al from '../images/logos/LPL/al.png'
+import edg from '../images/logos/LPL/edg.png'
+import ig from '../images/logos/LPL/ig.png'
+import jdg from '../images/logos/LPL/jdg.png'
+import lgd from '../images/logos/LPL/lgd.png'
+import nip from '../images/logos/LPL/nip.png'
+import omg from '../images/logos/LPL/omg.png'
+import ra from '../images/logos/LPL/ra.png'
+import rng from '../images/logos/LPL/rng.png'
+import tes from '../images/logos/LPL/tes.png'
+import tt from '../images/logos/LPL/tt.png'
+import up from '../images/logos/LPL/up.png'
+import we from '../images/logos/LPL/we.png'
+import weibo from '../images/logos/LPL/weibo.png'
+import blg from '../images/logos/LPL/blg.png'
+import lng from '../images/logos/LPL/lng.png'
+
+export const teams = [
+    // LCS
+    {
+        name: "Team Liquid",
+        logo: tl,
+    },
+    {
+        name: "100 Thieves",
+        logo: hundredThieves,
+    },
+    {
+        name: "Cloud9",
+        logo: c9,
+    },
+    {
+        name: "Counter Logic Gaming",
+        logo: clg,
+    },
+    {
+        name: "Team Dignitas",
+        logo: dig,
+    },
+    {
+        name: "Evil Geniuses",
+        logo: eg,
+    },
+    {
+        name: "FlyQuest",
+        logo: fly,
+    },
+    {
+        name: "Golden Guardians",
+        logo: gg,
+    },
+    {
+        name: "Immortals",
+        logo: imt,
+    },
+    {
+        name: "Team SoloMid",
+        logo: tsm,
+    },
+    // LEC
+    {
+        name: "MAD Lions",
+        logo: mad,
+    },
+    {
+        name: "G2 Esports",
+        logo: g2,
+    },
+    {
+        name: "SK Gaming",
+        logo: sk,
+    },
+    {
+        name: "Astralis",
+        logo: ast,
+    },
+    {
+        name: "Rogue",
+        logo: rge,
+    },
+    {
+        name: "Team Vitality",
+        logo: vit,
+    },
+    {
+        name: "Team BDS",
+        logo: bds,
+    },
+    {
+        name: "Misfits",
+        logo: msf,
+    },
+    {
+        name: "Fnatic",
+        logo: fnc,
+    },
+    {
+        name: "exceL eSports",
+        logo: xl,
+    },
+    // LCK
+    {
+        name: "Fredit BRION",
+        logo: bro,
+    },
+    {
+        name: "T1",
+        logo: t1,
+    },
+    {
+        name: "SANDBOX Gaming",
+        logo: lsb,
+    },
+    {
+        name: "Hanwha Life Esports",
+        logo: hle,
+    },
+    {
+        name: "kt Rolster",
+        logo: kt,
+    },
+    {
+        name: "Nongshim RF",
+        logo: ns,
+    },
+    {
+        name: "Gen.G",
+        logo: geng,
+    },
+    {
+        name: "DragonX",
+        logo: drx,
+    },
+    {
+        name: "afreeca Freecs",
+        logo: af,
+    },
+    {
+        name: "DAMWON Gaming",
+        logo: dwg,
+    },
+    // //  LPL
+    {
+        name: "Invictus Gaming",
+        logo: ig,
+    },
+    {
+        name: "FunPlus Phoenix",
+        logo: fpx,
+    },
+    {
+        name: "Oh My God",
+        logo: omg,
+    },
+    {
+        name: "Rare Atom",
+        logo: ra,
+    },
+    {
+        name: "EDward Gaming",
+        logo: edg,
+    },
+    {
+        name: "Bilibili Gaming",
+        logo: blg,
+    },
+    {
+        name: "Ultra Prime",
+        logo: up,
+    },
+    {
+        name: "LNG Esports",
+        logo: lng,
+    },
+    {
+        name: "TT",
+        logo: tt,
+    },
+    {
+        name: "JD Gaming",
+        logo: jdg,
+    },
+    {
+        name: "LGD Gaming",
+        logo: lgd,
+    },
+    {
+        name: "Team WE",
+        logo: we,
+    },
+    {
+        name: "Top Esports",
+        logo: tes,
+    },
+    {
+        name: "Anyone's Legend",
+        logo: al,
+    },
+    {
+        name: "Weibo Gaming",
+        logo: weibo,
+    },
+    {
+        name: "Ninjas in Pyjamas",
+        logo: nip,
+    },
+    {
+        name: "Royal Never Give Up",
+        logo: rng,
+    },
+]
